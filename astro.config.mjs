3e702ddc375a256c51713ffac188a07c24522e76
dennyraymondsprout <dennyraymondd@gmail.com>
2025-03-13 23:09:20 +0700

    feat: resetup vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
+// Remove the cloudflare import since you're using Vercel now
+// import cloudflare from "@astrojs/cloudflare";
 
-import vercel from "@astrojs/vercel";
+// Fix the Vercel import to specify which adapter you want to use
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
