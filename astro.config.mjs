0a9d6d0d20edc086bbf3398593a61333f558100a
raymondddenny <dennyraymondd@gmail.com>
2025-03-13 23:20:48 +0700

    feat: test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// Change from serverless to static adapter
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
