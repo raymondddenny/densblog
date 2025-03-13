cccbd6d4f67b016a9ba3957fa59217719f113934
dennyraymondsprout <dennyraymondd@gmail.com>
2025-03-13 22:24:40 +0700

    feat: init setup and first blog! wohoo

--- src/content/whoami/intro.md
+++ src/content/whoami/intro.md
@@ -1,32 +1,11 @@
 ---
-title: "meet sanju, designer founder"
-description: "I run ThisUX, a design-led product studio. I'm working on ideas like sticai.com, abogrow.com, uiino.com, supaprompts.com, ffmpeg.info, and more. Always tinkering."
-pubDate: 'Sept 1 2024'
+title: "meet denny, a software engineer"
+description: "I work as a software engineer in a digital consultant company. I like to build things that helpful for people, like to learn new things, and my favorite food is Bakso 🍜"
+pubDate: "Mar 13 2025"
 ---
 
-I run [ThisUX](https://thisux.com), a design-led product studio. We make things that work.
+Welcome to my corner of the internet! I'm Denny Raymond, a software engineer with a passion for building things that help people. I'm currently working as a software engineer in a digital consultant company.
 
-For years, I wore many hats. Now I'm doing my own thing.
+I love to learn new things, and I'm always up for a challenge. I'm also a foodie, and my favorite food is Bakso (meat balls from Indonesia).
 
-I'm working on ideas like:
-
-- [Dun](https://dunsuite.com)
-- [Fli](https://fli.so)
-- [Uiino](https://uiino.com)
-- [SticAi](https://sticai.com) 
-- [aboGrow](https://abogrow.com) 
-- [ffmpeg info](https://ffmpeg.info)
-- [Glance](https://glance.sticai.com)
-- [Spotlight](https://spotlight.thisux.com)
-
-
-
-And more. Always tinkering.
-
-I team up with founders and business owners. We build MVPs, full products, or fix broken experiences. Design to code, we do it all.
-
-Our secret? We create systems that work, no matter the tech or problem.
-
-I'm big on [open source](https://github.com/spikeysanju) too. I contribute code and speak at conferences.
-
-Want to chat? [Drop me a line](mailto:work@sanju.sh?subject=Let's%20Collaborate&body=Hi%20Sanju,%0D%0A%0D%0AI%20would%20like%20to%20discuss%20a%20potential%20collaboration%20with%20you.%0D%0A%0D%0ABest%20regards,%0D%0A%5BYour%20Name%5D).
+Want to chat? [Drop me a line](mailto:dennyraymondd@gmail.com?subject=Let's%20Collaborate&body=Hi%Denny,%0D%0A%0D%0AI%20would%20like%20to%20discuss%20a%20potential%20collaboration%20with%20you.%0D%0A%0D%0ABest%20regards,%0D%0A%5BYour%20Name%5D).
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,84 +1,82 @@
-export const SITE_TITLE = "Sanju's Space";
-export const SITE_DESCRIPTION = "A internet space for Sanju.";
+export const SITE_TITLE = "Den's Space";
+export const SITE_DESCRIPTION = "A internet space for Den.";
 
 export interface MenuItem {
-	label: string;
-	url: string;
+  label: string;
+  url: string;
 }
 
 export const menuItems: MenuItem[] = [
-	{
-		label: "home",
-		url: "/",
-	},
-	{
-		label: "writings",
-		url: "/writings",
-	},
-	{
-		label: "thoughts",
-		url: "/thoughts",
-	},
-	{
-		label: "ships",
-		url: "/ships",
-	},
+  {
+    label: "home",
+    url: "/",
+  },
+  {
+    label: "writings",
+    url: "/writings",
+  },
+  {
+    label: "thoughts",
+    url: "/thoughts",
+  },
+  {
+    label: "ships",
+    url: "/ships",
+  },
 ];
 
-export const title = "Sanju's Space";
-export const description = "A internet space for Sanju.";
+export const title = "Den's Space";
+export const description = "A internet space for Denny.";
 export const image = "/images/ogimage.png";
-export const url = "https://sanju.sh";
+export const url = "https://denncodes.com";
 
 export const ogImage = {
-	src: "/images/ogimage.png",
-	alt: "Sanju's Space",
+  src: "/images/ogimage.png",
+  alt: "Den's Space",
 };
 
-
 export const products = [
-	{
-		name: "Fli.so",
-		url: "https://fli.so",
-		image: "/products/fli.png",
-	},
-	{
-		name: "Uiino",
-		url: "https://uiino.com",
-		image: "/products/uiino.png",
-	},
-	{
-		name: "SticAI",
-		url: "https://sticai.com",
-		image: "/products/sticai.png",
-	},
-	{
-		name: "Dun",
-		url: "https://dunsuite.com",
-		image: "/products/dun.png",
-	},
-	{
-		name: "DunTasks",
-		url: "https://duntasks.com",
-		image: "/products/duntasks.png",
-	},
+  {
+    name: "Flutter",
+    url: "https://flutter.dev/",
+    image: "/products/flutterlogo.png",
+  },
+  {
+    name: "JS",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    image: "/products/jslogo.png",
+  },
+  {
+    name: "PostgreSql",
+    url: "https://www.postgresql.org/",
+    image: "/products/postgres.svg",
+  },
+  {
+    name: "Supabase",
+    url: "https://supabase.com/",
+    image: "/products/supabase.webp",
+  },
 ];
 
 export const socialLinks = [
-	{
-		label: "@x",
-		url: "https://x.com/spikeysanju",
-	},
-	{
-		label: "email",
-		url: "mailto:work@sanju.sh",
-	},
-	{
-		label: "github",
-		url: "https://github.com/spikeysanju",
-	},
-	{
-		label: "linkedin",
-		url: "https://www.linkedin.com/in/imsanju",
-	},
+  {
+    label: "@x",
+    url: "https://x.com/raymonddenny14",
+  },
+  {
+    label: "@threads",
+    url: "https://www.threads.net/@dennyraymond",
+  },
+  {
+    label: "email",
+    url: "mailto:dennyraymondd@gmail.com",
+  },
+  {
+    label: "github",
+    url: "https://github.com/raymondddenny",
+  },
+  {
+    label: "linkedin",
+    url: "https://www.linkedin.com/in/dennyraymondd/",
+  },
 ];
